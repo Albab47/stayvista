4193fbd8e1e43b0555fdd9fb7c0c8a9fb3a7c9f7
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-05-21 16:29:09 +0600

    first commit

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -1,9 +1,28 @@
-import PropTypes from 'prop-types'
+/* eslint-disable no-unused-vars */
+import PropTypes from "prop-types";
+import queryStr from "query-string";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const navigate = useNavigate();
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+
+  const handleClick = () => {
+    // Create query string
+    const currentQuery = { category: label };
+    const url = queryStr.stringifyUrl({
+      url: "/",
+      query: currentQuery,
+    });
+    // Set query to url
+    navigate(url);
+  };
+
   return (
     <div
-      className={`flex 
+      onClick={handleClick}
+      className={`flex
   flex-col 
   items-center 
   justify-center 
@@ -12,17 +31,19 @@ const CategoryBox = ({ label, icon: Icon }) => {
   border-b-2
   hover:text-neutral-800
   transition
-  cursor-pointer`}
+  cursor-pointer ${
+    label === category && "border-b-neutral-800 text-neutral-800 transition-all duration-500"
+  }`}
     >
       <Icon size={26} />
-      <div className='text-sm font-medium'>{label}</div>
+      <div className="text-sm font-medium">{label}</div>
     </div>
-  )
-}
+  );
+};
 
 CategoryBox.propTypes = {
   label: PropTypes.string,
   icon: PropTypes.elementType,
-}
+};
 
-export default CategoryBox
+export default CategoryBox;
